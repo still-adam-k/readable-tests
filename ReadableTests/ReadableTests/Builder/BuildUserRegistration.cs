dddda115722b0253537462b137c0afd8192213d7
Adam Kosinski <akosinski@paypoint.com>
2014-09-13 01:24:41 +0100

    build register service

---
+++ ReadableTests/ReadableTests/Builder/BuildUserRegistration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using TestsSwissArmyKnife;
+
+namespace ReadableTests.Builder
+{
+    class BuildUserRegistrationService
+    {
+        private UserAccount defaultUserToReturn;
+        private int forAccountReturn;
+
+        public BuildUserRegistrationService()
+        {
+            defaultUserToReturn = new UserAccount("", "", new DateTime(), "", "");
+        }
+
+        public BuildUserRegistrationService ForAccount(int id)
+        {
+            forAccountReturn = id;
+            return this;
+        }
+
+        public BuildUserRegistrationService Return(UserAccount user)
+        {
+            this.defaultUserToReturn = user;
+            return this;
+        }
+
+        private UserRegistrationService Build()
+        {
+            var service = new UserRegistrationService();
+            var mock = new Mock<IUserRepository>();
+            mock.Setup(o => o.GetUser(forAccountReturn)).Returns(defaultUserToReturn);
+            service.UsersRepository = mock.Object;
+            return service;
+        }
+
+        public static implicit operator UserRegistrationService(BuildUserRegistrationService builder)
+        {
+            return builder.Build();
+        }
+    }
+}
---
+++ ReadableTests/ReadableTests/RegistrationService_tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using ReadableTests.Builder;
+using TestsSwissArmyKnife;
+using Xunit;
+
+namespace ReadableTests
+{
+    public class GIVEN_UserRegistrationService
+    {
+        [Fact]
+        public void THEN_returns_user_for_that_id()
+        {
+            var expectedUser = new UserAccount("myName", "myEmail", DateTime.Parse("1990-10-10"), "123456789",
+                "password");
+
+            var service = new UserRegistrationService();
+            var mock = new Mock<IUserRepository>();
+            mock.Setup(o => o.GetUser(It.IsAny<int>())).Returns(expectedUser);
+            service.UsersRepository = mock.Object;
+
+            var returnedUser = service.GetUserInformation(123);
+
+            Assert.Equal(expectedUser, returnedUser);
+        }
+
+        [Fact]
+        public void THEN_returns_user_for_that_id_prettier()
+        {
+            var expectedUser = new UserAccount("myName", "myEmail", DateTime.Parse("1990-10-10"), "123456789",
+                "password");
+
+            UserRegistrationService service = new BuildUserRegistrationService()
+                .ForAccount(123)
+                .Return(expectedUser);
+
+            var returnedUser = service.GetUserInformation(123);
+
+            Assert.Equal(expectedUser, returnedUser);
+        }
+
+    }
+}
